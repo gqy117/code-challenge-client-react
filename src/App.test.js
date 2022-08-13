2d2877949538de88e1fb2d4e8867a5c92cb1cb67
Leo Li <gqy117@gmail.com>
2022-08-13 22:44:05 +1200

    ✨ Challenge complete.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,59 @@
-import {render} from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
+const data = [
+    {
+        id: '1',
+        name: 'Pilsner',
+        minimumTemperature: 4,
+        maximumTemperature: 6,
+    },
+    {
+        id: '2',
+        name: 'IPA',
+        minimumTemperature: 5,
+        maximumTemperature: 6,
+    },
+    {
+        id: '3',
+        name: 'Lager',
+        minimumTemperature: 4,
+        maximumTemperature: 7,
+    },
+    {
+        id: '4',
+        name: 'Stout',
+        minimumTemperature: 6,
+        maximumTemperature: 8,
+    },
+    {
+        id: '5',
+        name: 'Wheat beer',
+        minimumTemperature: 3,
+        maximumTemperature: 5,
+    },
+    {
+        id: '6',
+        name: 'Pale Ale',
+        minimumTemperature: 4,
+        maximumTemperature: 6,
+    },
+];
+
 describe('<App />', () => {
-    it('renders without errors', () => {
-        render(<App />)
+    it('renders without errors', async () => {
+        const { container } = render(<App />);
+        await new Promise((r) => setTimeout(r, 4000));
+
+        data.forEach(({ id, name, minimumTemperature, maximumTemperature }) => {
+            const product = container.querySelector(`tbody > :nth-child(${id}) > :nth-child(1)`).innerHTML;
+            const temperature = container.querySelector(`tbody > :nth-child(${id}) > :nth-child(2)`).innerHTML;
+            const status = container.querySelector(`tbody > :nth-child(${id}) > :nth-child(3) > span`).innerHTML;
+            
+            const expected = temperature < minimumTemperature ? 'too low' : temperature > maximumTemperature ? 'too high' : 'all good';
+
+            expect(product).toBe(name);
+            expect(status).toBe(expected);
+        });
     });
 });
